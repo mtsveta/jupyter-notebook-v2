5f748a00c65f509fa19e755e288826a84a1f69ec
Svetlana Kyas <mtsveta@gmail.com>
2021-11-10 14:29:02 +0100

    Remove comments from the scripts

--- scripts/ex-expert-equilibrium-unknown-standard-chemical-potential.py
+++ scripts/ex-expert-equilibrium-unknown-standard-chemical-potential.py
@@ -11,33 +11,6 @@
 #       jupytext_version: 1.13.1
 # ---
 
-# Reaktoro is a unified framework for modeling chemically reactive systems.
-#
-# Copyright © 2014-2021 Allan Leal
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License
-# along with this library. If not, see <http://www.gnu.org/licenses/>.
-
-# -----------------------------------------------------------------------------
-# 👏 Acknowledgements 👏
-# -----------------------------------------------------------------------------
-# This example was originally authored by:
-#   • Allan Leal (4 August 2021)
-#
-# and since revised by:
-#   •
-# -----------------------------------------------------------------------------
-
 from reaktoro import *
 from autodiff import abs
 
