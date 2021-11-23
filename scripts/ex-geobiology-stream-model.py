7526de12cf0b2e9f65ed09a8731eada7e26a796c
Svetlana Kyas <mtsveta@gmail.com>
2021-11-23 15:58:40 +0100

    Double loop for the StreamModel example

--- scripts/ex-geobiology-stream-model.py
+++ scripts/ex-geobiology-stream-model.py
@@ -11,6 +11,11 @@
 # ---
 
 from reaktoro import *
+import numpy as np
+import os
+
+results_folder = 'results-stream-model'
+os.system('mkdir -p ' + results_folder)
 
 db = PhreeqcDatabase.fromFile('../databases/phreeqc-extended.dat') # if running from tutorials folder
 
@@ -18,7 +23,7 @@ print("Database content:\n---------------------")
 for species in db.species():
     print(species.name())
 
-solution = AqueousPhase(speciate("H O C Na Cl"))
+solution = AqueousPhase(speciate("H O C Na Cl P"))
 solution.setActivityModel(chain(
     ActivityModelHKF(),
     ActivityModelDrummond("CO2")
@@ -31,25 +36,62 @@ minerals = MineralPhases("Fluorapatite Hydroxylapatite Calcite")
 
 system = ChemicalSystem(db, solution, gases, minerals)
 
-T = 25.0 # temperature in celsius
-P = 1.0  # pressure in bar
-
-state = ChemicalState(system)
-state.temperature(T, "celsius")
-state.pressure(P, "bar")
-state.setSpeciesMass("H2O"     , 1.0 , "kg")
-state.setSpeciesAmount("CO2(g)", 10.0, "mol")
-state.setSpeciesAmount("Na+"   , 4.00, "mol")
-state.setSpeciesAmount("Cl-"   , 4.00, "mol")
-state.setSpeciesAmount("Calcite"        , 10.00, "mol")
-state.setSpeciesAmount("Fluorapatite"   , 10.00, "mol")
-state.setSpeciesAmount("Hydroxylapatite", 10.00, "mol")
+print("System content:\n---------------------")
+for species in db.species():
+    print(species.name())
 
 solver = EquilibriumSolver(system)
-solver.solve(state)
 
-props = ChemicalProps(state)
-print(props)
+props = ChemicalProps(system)
+aprops = AqueousProps(system)
 
-aprops = AqueousProps(state)
-print(aprops)
+def convertPPCO2Bar(ppCO2):
+    return 10**ppCO2 # log10(P) = ppCO2
+
+def equilibrate(T, ppCO2):
+
+    P = convertPPCO2Bar(ppCO2)
+    state = ChemicalState(system)
+    state.temperature(T, "celsius")
+    state.pressure(P, "atm")
+    state.setSpeciesMass("H2O"     , 1.0 , "kg")
+    state.setSpeciesAmount("CO2(g)", 100.0, "mmol")
+    state.setSpeciesAmount("Na+"   , 4.00, "mmol")
+    state.setSpeciesAmount("Cl-"   , 4.00, "mmol")
+    state.setSpeciesAmount("Calcite"        , 10.00, "mmol")
+    state.setSpeciesAmount("Fluorapatite"   , 10.00, "mmol")
+    state.setSpeciesAmount("Hydroxylapatite", 10.00, "mmol")
+
+    solver.solve(state)
+
+    props.update(state)
+    aprops.update(state)
+
+    # PO4 - 3
+    # H2PO4 -
+    # CaH2PO4 +
+    # CaHCO3 +
+    # HPO4 - 2
+
+    mPO4 = state.speciesAmount("HPO4-2")[0]
+
+    return mPO4
+
+
+num_temperatures = 101
+num_co2s = 106
+temperatures = np.linspace(0, 50.0, num=num_temperatures)
+co2ppressures = np.linspace(-4.1, 0.1, num=num_co2s)
+
+mols_PO4 = np.zeros((num_temperatures, num_co2s))
+
+print(temperatures)
+print(co2ppressures)
+
+t_couter = 0
+for T in temperatures:
+    mols_PO4[t_couter, :] = np.array([equilibrate(T, ppCO2) for ppCO2 in co2ppressures])
+    t_couter += 1
+
+np.savetxt(results_folder + '/m-PO4.txt', mols_PO4)
+
