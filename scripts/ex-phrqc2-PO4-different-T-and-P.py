f6c671b290195704f35e8372150cdc45d1eed552
Svetlana Kyas <mtsveta@gmail.com>
2021-12-15 20:14:03 +0100

    Adding more detailed naming of the equilibration functions and plotting more documented data

--- scripts/ex-phrqc2-PO4-different-T-and-P.py
+++ scripts/ex-phrqc2-PO4-different-T-and-P.py
@@ -80,7 +80,7 @@ opts.epsilon = 1e-13
 solver.setOptions(opts)
 
 
-def equilibrate_pure(T, ppCO2):
+def equilibrate_Na_HPO4(T, ppCO2):
 
     conditions.temperature(T, "celsius")
     conditions.pressure(1.0, "bar")
@@ -111,7 +111,50 @@ def equilibrate_pure(T, ppCO2):
     else:
         return pH, moleP
 
-def equilibrate_mixed(T, ppCO2):
+def equilibrate_Na_HCO3_CO3_HPO4(T, ppCO2):
+
+    conditions.temperature(T, "celsius")
+    conditions.pressure(1.0, "bar")
+    conditions.fugacity("CO2", 10**(ppCO2), 'bar')
+
+    state = ChemicalState(system)
+    state.set("H2O", 1.0, "kg")
+    state.set("CO2", 100, "mol")
+
+    # All the Phosphat species
+    state.set("Na2(HPO4):12H2O", 10.00, "mol")
+    #state.set("Na2(HPO4):7H2O", 10.00, "mol")
+    # state.set("Na2(HPO4):2H2O", 10.00, "mol") # solver doesn't converge
+    # state.set("Na2(HPO4)", 10.00, "mol")
+    # state.set("Na(H2PO4):2H2O", 10.00, "mol")
+    # state.set("Na(H2PO4):H2O", 10.00, "mol")
+    # state.set("Na(H2PO4)", 10.00, "mol")
+
+    # All the sodium minerals
+    #state.set("Natron", 10.00, "mol")       # Na2CO3:10H2O
+    state.set("Nahcolite", 10.00, "mol")   # NaHCO3 (works better then Natron)
+    #state.set("Trona", 10.00, "mol")       # Na3H(CO3)2:2H2O (convergese the worst of the above)
+    #state.set("Na2CO3:H2O", 10.00, "mol")
+    #state.set("Na2CO3:7H2O", 10.00, "mol")
+
+    res = solver.solve(state, conditions)
+
+    if not res.optima.succeeded:
+        print(f"ERROR: The optimization solver hasn't converged for T = {T} C and ppCO2 = {ppCO2}")
+        return nan, nan
+
+    props.update(state)
+    aprops.update(state)
+
+    pH = aprops.pH()[0]
+    moleP = props.elementAmountInPhase("P", "AqueousPhase")[0]
+
+    if pH < 5 or pH > 13:
+        return nan, nan
+    else:
+        return pH, moleP
+
+def equilibrate_Na_Cl_HCO3_CO3_HPO4(T, ppCO2):
 
     conditions.temperature(T, "celsius")
     conditions.pressure(1.0, "bar")
@@ -166,7 +209,7 @@ co2ppressures = np.linspace(-3.5, 0.0, num=num_ppressures)
 #
 # for i in range(0, num_temperatures):
 #     # ppCO2 = -3.5
-#     result = equilibrate_pure(temperatures[i], co2ppressures[0])
+#     result = Na_HPO4(temperatures[i], co2ppressures[0])
 #     #print(result)
 #     #input()
 #     if result != "ERROR":
@@ -175,7 +218,7 @@ co2ppressures = np.linspace(-3.5, 0.0, num=num_ppressures)
 #         data35[i, 2] = result[1]
 #
 #     # ppCO2 = 0.0
-#     result = equilibrate_pure(temperatures[i], co2ppressures[1])
+#     result = Na_HPO4(temperatures[i], co2ppressures[1])
 #     #input()
 #     if result != "ERROR":
 #         data0[i, 0] = temperatures[i]
@@ -210,6 +253,59 @@ co2ppressures = np.linspace(-3.5, 0.0, num=num_ppressures)
 # plt.savefig(results_folder + '/' + 'moleP-vs-ppCO2-pure-Na2(HPO4)-12H2O.png', bbox_inches='tight')
 # plt.close()
 
+# # ##########################################################
+#
+# data_size = 2
+# data0  = np.zeros((num_temperatures, data_size+1))
+# data35 = np.zeros((num_temperatures, data_size+1))
+#
+# for i in range(0, num_temperatures):
+#     # ppCO2 = -3.5
+#     result = equilibrate_Na_Cl_HCO3_CO3_HPO4(temperatures[i], co2ppressures[0])
+#     #print(result)
+#     #input()
+#     if result != "ERROR":
+#         data35[i, 0] = temperatures[i]
+#         data35[i, 1] = result[0]
+#         data35[i, 2] = result[1]
+#
+#     # ppCO2 = 0.0
+#     result = equilibrate_Na_Cl_HCO3_CO3_HPO4(temperatures[i], co2ppressures[1])
+#     #input()
+#     if result != "ERROR":
+#         data0[i, 0] = temperatures[i]
+#         data0[i, 1] = result[0]
+#         data0[i, 2] = result[1]
+#
+# np.savetxt(results_folder + '/data0-mixed-Na2(HPO4)-7H2O.txt', data0)
+# np.savetxt(results_folder + '/data35-mixed-Na2(HPO4)-7H2O.txt', data35)
+#
+# import matplotlib.pyplot as plt
+# colors = ['C1', 'C2', 'C3', 'C4', 'C5', 'C7', 'C8', 'C9']
+#
+# plt.figure()
+# plt.plot(temperatures, data0[:, 1], label=f'ppCO2 = 0', color=colors[0])
+# plt.plot(temperatures, data35[:, 1], label=f'ppCO2 = -3.5', color=colors[1])
+#
+# plt.legend(loc="best")
+# plt.xlabel('T [degC]')
+# plt.ylabel('pH [-]')
+# plt.grid()
+# plt.savefig(results_folder + '/' + 'pH-vs-ppCO2-mixed-Na2(HPO4)-7H2O.png', bbox_inches='tight')
+# plt.close()
+#
+# plt.figure()
+# plt.plot(temperatures, data0[:, 2], label=f'ppCO2 = 0', color=colors[2])
+# plt.plot(temperatures, data35[:, 2], label=f'ppCO2 = -3.5', color=colors[3])
+# plt.yscale('log')
+# plt.legend(loc="best")
+# plt.xlabel('T [degC]')
+# plt.ylabel('Amount of P [mole]')
+# plt.grid()
+# plt.savefig(results_folder + '/' + 'moleP-vs-ppCO2-mixed-Na2(HPO4)-7H2O.png', bbox_inches='tight')
+# plt.close()
+#
+
 # ##########################################################
 
 data_size = 2
@@ -218,7 +314,7 @@ data35 = np.zeros((num_temperatures, data_size+1))
 
 for i in range(0, num_temperatures):
     # ppCO2 = -3.5
-    result = equilibrate_mixed(temperatures[i], co2ppressures[0])
+    result = equilibrate_Na_HCO3_CO3_HPO4(temperatures[i], co2ppressures[0])
     #print(result)
     #input()
     if result != "ERROR":
@@ -227,15 +323,15 @@ for i in range(0, num_temperatures):
         data35[i, 2] = result[1]
 
     # ppCO2 = 0.0
-    result = equilibrate_mixed(temperatures[i], co2ppressures[1])
+    result = equilibrate_Na_HCO3_CO3_HPO4(temperatures[i], co2ppressures[1])
     #input()
     if result != "ERROR":
         data0[i, 0] = temperatures[i]
         data0[i, 1] = result[0]
         data0[i, 2] = result[1]
 
-np.savetxt(results_folder + '/data0-mixed-Na2(HPO4)-7H2O.txt', data0)
-np.savetxt(results_folder + '/data35-mixed-Na2(HPO4)-7H2O.txt', data35)
+np.savetxt(results_folder + '/data0-mixed-NaHCO3_CO3_HPO4.txt', data0)
+np.savetxt(results_folder + '/data35-mixed-NaHCO3_CO3_HPO4.txt', data35)
 
 import matplotlib.pyplot as plt
 colors = ['C1', 'C2', 'C3', 'C4', 'C5', 'C7', 'C8', 'C9']
@@ -248,7 +344,7 @@ plt.legend(loc="best")
 plt.xlabel('T [degC]')
 plt.ylabel('pH [-]')
 plt.grid()
-plt.savefig(results_folder + '/' + 'pH-vs-ppCO2-mixed-Na2(HPO4)-7H2O.png', bbox_inches='tight')
+plt.savefig(results_folder + '/' + 'pH-vs-ppCO2-NaHCO3_CO3_HPO4.png', bbox_inches='tight')
 plt.close()
 
 plt.figure()
@@ -259,5 +355,5 @@ plt.legend(loc="best")
 plt.xlabel('T [degC]')
 plt.ylabel('Amount of P [mole]')
 plt.grid()
-plt.savefig(results_folder + '/' + 'moleP-vs-ppCO2-mixed-Na2(HPO4)-7H2O.png', bbox_inches='tight')
+plt.savefig(results_folder + '/' + 'moleP-vs-ppCO2-NaHCO3_CO3_HPO4.png', bbox_inches='tight')
 plt.close()
